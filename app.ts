c209e33cb16dc4b1ef6cfdc2fb2034879f040178
oppakuu <bffkirilin@gmail.com>
2024-07-01 14:41:13 -0500

    init

---
+++ app.ts
@@ -0,0 +1,70 @@
+class Meditation {
+    constructor(private count: number) {
+    }
+
+    start() {
+        setTimeout(() => {
+            for (let i = this.count; i > 0; i--) {
+                console.log(i);
+            }
+            console.log("Jay Guru Dev");
+        }, 0)
+    }
+}
+
+const morning_meditation = new Meditation(5);
+morning_meditation.start();
+console.log(`Start meditation`);
+
+const isPrime = (n: number) => {
+    return new Promise((resolve, reject) => {
+        let isPrime: boolean = n > 1;
+        for (let i = 2, s = Math.sqrt(n); i <= s; i++)
+            if (n % i === 0) isPrime = false;
+
+        if (isPrime) {
+            resolve({prime: true});
+        }
+        reject({prime: false});
+    })
+};
+
+console.log('start');
+isPrime(7)
+    .then(console.log)
+    .catch(console.error);
+console.log('end');
+
+interface Root {
+    recipes: Recipe[]
+    total: number
+    skip: number
+    limit: number
+}
+
+interface Recipe {
+    id: number
+    name: string
+    ingredients: string[]
+    instructions: string[]
+    prepTimeMinutes: number
+    cookTimeMinutes: number
+    servings: number
+    difficulty: string
+    cuisine: string
+    caloriesPerServing: number
+    tags: string[]
+    userId: number
+    image: string
+    rating: number
+    reviewCount: number
+    mealType: string[]
+}
+
+(async () => {
+    const response = await fetch('https://dummyjson.com/recipes?limit=5');
+    const root: Root = await response.json();
+    for(const recipe of root.recipes) {
+        console.log(`${recipe.id}: Recipe name - ${recipe.name}`);
+    }
+}) ();
